634d956fb8a7750977077a34842bfe641bade88d
Emil Sundberg <Emil_djuplunda@hotmail.com>
2016-04-04 12:01:54 +0200

    byggt på ShowCard

--- GalaxyCards/src/guiPacket/ShowCardGUI.java
+++ GalaxyCards/src/guiPacket/ShowCardGUI.java
@@ -3,8 +3,29 @@ package guiPacket;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
-
+/**
+ * GUI class that contains events for when a card is hovered over.
+ *  The last card hovered over will be shown in a panel.
+ * @author emilsundberg
+ *
+ */
 public class ShowCardGUI extends JPanel{
+	private JPanel cardPanel;
+	
+	/**
+	 * recieves a Card object which is then displayed in a panel
+	 * @param card - the card to show
+	 */
+	private void showCard(Card card) {
+		
+	}
+	
+	public void MouseEntered(MouseEvent e) {
+		
+		if(e.getSource() instanceof Card ) {
+			
+		}
+	}
 	
 	
 
