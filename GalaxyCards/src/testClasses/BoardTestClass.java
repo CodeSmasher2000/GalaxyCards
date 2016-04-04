f4676924bb8b2da9186ae4db87be697af1f1c0a8
13120dde <13120dde@gmail.com>
2016-04-05 01:25:02 +0200

    Skapat en klass som motsvarar en grafisk array
    Man ska kunna använda klassen för samtliga fält på spelplanen. I klassen
    lagras Unit objekt i vars sin layeredPanel. Det är tänkt att feedback
    ska ritas över Unit objektet på ett nytt lager.

--- GalaxyCards/src/testClasses/BoardTestClass.java
+++ GalaxyCards/src/testClasses/BoardTestClass.java
@@ -7,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,6 +16,7 @@ import javax.swing.JPanel;
 import cards.Deck;
 import exceptionsPacket.EmptyDeckException;
 import exceptionsPacket.NoPlaceOnBoardException;
+import guiPacket.ArrayLayeredPane;
 import guiPacket.BoardGuiController;
 import guiPacket.Card;
 import guiPacket.HandGUI;
@@ -23,14 +26,22 @@ import guiPacket.HeroicPanelGUI;
 public class BoardTestClass {
 
 	private BoardGuiController board = new BoardGuiController();
-	private HandGUI hand;
-	private HeroicPanelGUI hp;
-	private HeroGUI hero;
+	private BoardGuiController board2 = new BoardGuiController();
+	private HandGUI hand, hand2;
+	private HeroicPanelGUI hp, hp2;
+	private HeroGUI hero, hero2;
 	private Deck deck;
 	private ObjectInputStream ois;
+	private ArrayLayeredPane playerDefensiveLane = new ArrayLayeredPane(6);
+	private ArrayLayeredPane playerOffensive = new ArrayLayeredPane(6);
+	private ArrayLayeredPane enemyDefensiveLane = new ArrayLayeredPane(6);
+	private ArrayLayeredPane enemyOffensiveLane = new ArrayLayeredPane(6);
 	
 	private JPanel panelGUI = new JPanel();
+	private JPanel enemyGui = new JPanel();
 	private JPanel panelBTN = new JPanel();
+	private JPanel mainPanel = new JPanel();
+	
 	private JButton draw = new JButton("Dra kort");
 	private Card temp;	
 	
@@ -38,13 +49,33 @@ public class BoardTestClass {
 		hand = new HandGUI(board);
 		hp = new HeroicPanelGUI(board);
 		hero = new HeroGUI(board);
+		hand2 = new HandGUI(board2);
+		hp2 = new HeroicPanelGUI(board2);
+		hero2 = new HeroGUI(board2);
 		
 		draw.addActionListener(new ButtonListener());
+//		playerDefensiveLane.setContainerName("Defensive Lane");
+//		playerOffensive.setContainerName("Offensive Lane");
+//		enemyDefensiveLane.setContainerName("Enemy's Defensive Lane");
+//		enemyOffensiveLane.setContainerName("Enemy's Offensive Lane");
 		
+		enemyGui.add(hand2);
+		enemyGui.add(hero2);
+		enemyGui.add(hp2);
 		panelGUI.add(hand);
 		panelGUI.add(hero);
 		panelGUI.add(hp);
 		
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanel.add(enemyGui);
+		mainPanel.add(enemyDefensiveLane);
+		mainPanel.add(Box.createVerticalStrut(2));
+		mainPanel.add(enemyOffensiveLane);
+		mainPanel.add(Box.createVerticalStrut(2));
+		mainPanel.add(playerOffensive);
+		mainPanel.add(Box.createVerticalStrut(2));
+		mainPanel.add(playerDefensiveLane);
+		mainPanel.add(panelGUI);
 		panelBTN.add(draw);
 		
 		try {
@@ -67,7 +98,7 @@ public class BoardTestClass {
 	
 	public void showUI(){
 		JFrame frame = new JFrame();
-		frame.add(panelGUI);
+		frame.add(mainPanel);
 		frame.setVisible(true);
 		frame.pack();
 		
