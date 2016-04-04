dc23232417333d55a2eee528863679d4501a3892
CodeSmasher2000 <jonatanwahlstedt@gmail.com>
2016-04-04 11:25:18 +0200

    Skapade första Deck och lade till description-funktion

--- GalaxyCards/src/Server/Server.java
+++ GalaxyCards/src/Server/Server.java
@@ -3,4 +3,8 @@ package Server;
 public class Server {
 	private Connection connection;
 	private ClientHandler clientHandler;
+	
+	public static void main(String[] args) {
+		new Connection(3550);
+	}
 }
