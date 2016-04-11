3e23da829e7400d1daf193d2a1bc8a5f8918bdfc
Patrik Brodelin <13120dde@gmail.com>
2016-04-11 10:21:08 +0200

    klassdiagram

---
+++ GalaxyCards/src/exceptionsPacket/InsufficientResourcesException.java
@@ -0,0 +1,9 @@
+package exceptionsPacket;
+
+public class InsufficientResourcesException extends Exception {
+	
+	public InsufficientResourcesException(String message){
+		super(message);
+	}
+
+}
--- GalaxyCards/src/game/Hero.java
+++ GalaxyCards/src/game/Hero.java
@@ -2,9 +2,7 @@ package game;
 
 import java.io.Serializable;
 
-import javax.swing.JOptionPane;
-
-import guiPacket.HeroGUI;
+import javax.naming.InsufficientResourcesException;
 
 /**
  * This class represents the player's hero and is responsible for a logical
@@ -25,6 +23,7 @@ public class Hero implements Serializable {
 	 */
 	private static final long serialVersionUID = -704227521994333558L;
 	private int life, energyShield, maxResource, currentResource;
+	private boolean resourcecardPlayed = false;
 
 	public Hero(String heroName) {
 		life = 20;
@@ -39,28 +38,21 @@ public class Hero implements Serializable {
 	 * If the damage is higher than the amount of energy shield, any excessive
 	 * damage won't go trough to the hero's life.
 	 * 
-	 * @param damage
+	 * @param amount
 	 */
-	public void dealDamage(int damage) {
+	public void dealDamage(int amount) {
 		if (energyShield > 0) {
-			energyShield -= damage;
-			
-			//Debugg
-//			updateShiledBar(energyShield);
+			energyShield -= amount;
+
 		} else {
 			energyShield = 0;
-			life -= damage;
-			
-			//Debugg
-//			updateLifeBar(life);
-//			updateShiledBar(energyShield);
+			life -= amount;
+
+			// Debugg
+			// updateLifeBar(life);
+			// updateShiledBar(energyShield);
 		}
 
-		// TODO update gui
-		if (life <= 0) {
-			youLoose();
-			JOptionPane.showMessageDialog(null, "YOU LOOSE!");
-		}
 	}
 
 	/**
@@ -74,10 +66,11 @@ public class Hero implements Serializable {
 		if (energyShield > 10) {
 			energyShield = 10;
 		}
-		
-		//DEBUGG
-//		updateShiledBar(energyShield);
+
+		// DEBUGG
+		// updateShiledBar(energyShield);
 	}
+	
 
 	/**
 	 * Attempts to remove x amount of energy shield from the hero. If x is
@@ -90,43 +83,46 @@ public class Hero implements Serializable {
 	public boolean removeShield(int amount) {
 		if (energyShield == 0 || energyShield < amount) {
 			return false;
+			// throw exception that is catched in the object which tries to
+			// activate the ability
 		} else {
 			energyShield -= amount;
-			
-			//DEBUGG
-//			updateShiledBar(energyShield);
+
+			// DEBUGG
+			// updateShiledBar(energyShield);
 			return true;
 		}
 
 	}
 
-	public void setCurrentResource(int amount) {
-		currentResource = amount;
-		if(currentResource>maxResource){
-			currentResource=maxResource;
+	public int getLife(){
+		return life;
+	}
+	
+	public int getEnergyShield(){
+		return energyShield;
+	}
+	
+	public void addResource(){
+		if(!resourcecardPlayed){
+		maxResource++;
+		currentResource++;
+		}else{
+			throw new ResourcePlayedException("You can only play one resource card each turn");
 		}
 	}
-
-	public int getCurrentResource() {
-		return currentResource;
+	
+	public void useResource(int amount) throws InsufficientResourcesException{
+		if(amount>currentResource){
+			throw new InsufficientResourcesException("Insufficient resources");
+		}
+		currentResource-=amount;
 	}
-
-	/**
-	 * Increments the max amount of resources. int = 1 should be passed in as
-	 * argument.
-	 * 
-	 * @param amount
-	 *            : int
-	 */
-	public void setMaxResource(int amount) {
-		currentResource+=amount;
-		maxResource += amount;
+	
+	public void resetResources(){
+		currentResource=maxResource;
 	}
-
-	public int getMaxResource() {
-		return maxResource;
-	}
-
+	
 	public String toString() {
 		return "life: " + life + "\nShield: " + energyShield + "\n current resource: " + currentResource + " / "
 				+ maxResource;
