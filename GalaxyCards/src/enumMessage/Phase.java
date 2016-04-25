1b320b2fff33ee225ea45e6474983c35f4a46a6e
Patrik Larsson <patriklarsson@gmail.com>
2016-04-25 13:16:38 +0200

    Added a phase object

---
+++ GalaxyCards/src/enumMessage/Phase.java
@@ -0,0 +1,12 @@
+package enumMessage;
+
+/**
+ * Conatins the diffrent phases the players in the game can be.
+ * @author patriklarsson
+ *
+ */
+public enum Phase {
+	IDLE,
+	ATTACKING,
+	DEFENDING;
+}
