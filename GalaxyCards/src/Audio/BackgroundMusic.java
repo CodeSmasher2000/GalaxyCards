69f8045f27648c6459e3b09fdc3436ef299f0f9f
CodeSmasher2000 <jonatanwahlstedt@gmail.com>
2016-04-27 09:31:05 +0200

    Ändrade Backgroundmusic

--- GalaxyCards/src/Audio/BackgroundMusic.java
+++ GalaxyCards/src/Audio/BackgroundMusic.java
@@ -7,18 +7,31 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 
-public class BackgroundMusic {
+public class BackgroundMusic implements Runnable {
 	private AudioClip clientBackground = null;
 	
-	public void clientMusic(){
+	
+//	public void clientMusic(){
+//		try {
+//			URL url = new File("C:/lol/GalxyCardsSoundTrack.wav").toURI().toURL();
+//			clientBackground = Applet.newAudioClip(url);
+//		} catch (MalformedURLException e) {
+//		}
+//
+//		if (clientBackground != null) {
+//			clientBackground.play();
+//		}
+//	}
+	@Override
+	public void run() {
 		try {
-			URL url = new File("C:/lol/.wav").toURI().toURL();
+			URL url = new File("C:/lol/GalxyCardsSoundTrack.wav").toURI().toURL();
 			clientBackground = Applet.newAudioClip(url);
 		} catch (MalformedURLException e) {
 		}
 
 		if (clientBackground != null) {
 			clientBackground.play();
-		}
+		}		
 	}
 }
